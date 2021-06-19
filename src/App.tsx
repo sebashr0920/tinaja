a0d794e95aa1349975b5f30c9b619da2e956e902
Sebastian Hernandez <shernandez@veritran.com>
2021-06-19 11:55:14 -0500

    adds basic info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+const linkWapp = 'https://api.whatsapp.com/send?phone=+573103822900&text=Hola!%20Quiero%20m%C3%A1s%20info%20acerca%20de%20';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Sitio web en construcción...</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>
+          El Agua es vida y la Vida es Amor.
+        </h1>
+        <p>Personal 250 ml.</p>
+        <p>Agua para hielo 520 ml.</p>
+        <p>Hogar 2200 ml.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={linkWapp}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contáctanos
         </a>
       </header>
     </div>
